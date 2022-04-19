c283e05bf2751b1c98e5b9f29c69997b3ab18049
Valkyrie Felso <vmfelso@users.noreply.github.com>
2022-04-19 23:01:29 +0200

    really simple solution for #10

--- static/js/survey.js
+++ static/js/survey.js
@@ -44,24 +44,60 @@ $(document).ready(function () {
 
 
     $("#add_Familie").on('click', function() {
-        if (($('.form-group-member').length == 1) && $('.form-group-member').is(":hidden")){
-            $('.form-group-member:last').show()
-            $('.form-group-member:last').attr('hidden',false);
+        if (($('#Familie .form-group-member').length == 1) && $('#Familie .form-group-member').is(":hidden")){
+            $('#Familie .form-group-member').show()
+            $('#Familie .form-group-member').attr('hidden',false);
         } else {
-        $('.form-group-member:last').clone().insertAfter('.form-group-member:last')
+        $('#Familie .form-group-member:last').clone().insertAfter('#Familie .form-group-member:last')
         }
     })
 
     $("#Familie").on('click', "#delete_Familie", function(e) {
         e.preventDefault();
-        if ($('.form-group-member').length == 1){
-            $(this).closest('.form-group-member').hide();
-            $(this).closest('.form-group-member').attr('hidden',true);
+        if ($('#Familie .form-group-member').length == 1){
+            $(this).closest('#Familie .form-group-member').hide();
+            $(this).closest('#Familie .form-group-member').attr('hidden',true);
         } else {
-            $(this).closest('.form-group-member').remove();
+            $(this).closest('#Familie .form-group-member').remove();
         }
     })
 
+    $("#add_Stationen").on('click', function() {
+        if (($('#Stationen  .form-group-member').length == 1) && $('#Stationen  .form-group-member').is(":hidden")){
+            $('#Stationen  .form-group-member').show()
+            $('#Stationen  .form-group-member').attr('hidden',false);
+        } else {
+        $('#Stationen  .form-group-member:last').clone().insertAfter('#Stationen  .form-group-member:last')
+        }
+    })
 
+    $("#Stationen").on('click', "#delete_Stationen", function(e) {
+        e.preventDefault();
+        if ($('#Stationen .form-group-member').length == 1){
+            $(this).closest('#Stationen .form-group-member').hide();
+            $(this).closest('#Stationen .form-group-member').attr('hidden',true);
+        } else {
+            $(this).closest('#Stationen .form-group-member').remove();
+        }
+    })
+
+    $("#add_Flucht").on('click', function() {
+        if (($('#Flucht .form-group-member').length == 1) && $('#Flucht .form-group-member').is(":hidden")){
+            $('#Flucht .form-group-member').show()
+            $('#Flucht .form-group-member').attr('hidden',false);
+        } else {
+        $('#Flucht .form-group-member:last').clone().insertAfter('#Flucht .form-group-member:last')
+        }
+    })
+
+    $("#Flucht").on('click', "#delete_Flucht", function(e) {
+        e.preventDefault();
+        if ($('#Flucht .form-group-member').length == 1){
+            $(this).closest('#Flucht .form-group-member').hide();
+            $(this).closest('#Flucht .form-group-member').attr('hidden',true);
+        } else {
+            $(this).closest('#Flucht .form-group-member').remove();
+        }
+    })
 });
 
